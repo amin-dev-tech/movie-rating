f5be445d2c1345b24e4a36104ff69e3fe1b2493f
amin-dev-tech <amin.dev.tech@gmail.com>
2023-09-11 11:52:35 +0300

    movie rating app

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>this movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+{
+  /* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={5} /> */
+}
+
+{
+  /* <Test /> */
+}
